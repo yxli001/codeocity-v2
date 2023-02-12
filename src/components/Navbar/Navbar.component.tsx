41367a20e615186da6586dbf14fc2415e493309b
Brian Mason <brian.kw.mason@gmail.com>
2023-02-11 23:15:57 -0500

    Animated burger

--- src/components/Navbar/Navbar.component.tsx
+++ src/components/Navbar/Navbar.component.tsx
@@ -5,45 +5,49 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import useWindowDimensions from "../../utils/Dimensions.utils.js";
 
 const Navbar = () => {
-    const [displayNavbar, setDisplayNavbar] = useState(false);
-    const { height, width } = useWindowDimensions();
+  const [displayNavbar, setDisplayNavbar] = useState(false);
+  const { height, width } = useWindowDimensions();
 
-    return (
-        <div className={style.container}>
-            <div className={style.upperRow}>
-                <Link className={`${style.logo}`} to="/">
-                    Codeocity
-                </Link>
-                <div
-                    className={style.burger}
-                    onClick={() => {
-                        setDisplayNavbar(!displayNavbar);
-                        // handle animation
-                    }}
-                >
-                    <div className={style.line1}></div>
-                    <div className={style.line2}></div>
-                    <div className={style.line3}></div>
-                </div>
-            </div>
-            {width > 825 || displayNavbar ? (
-                <div className={`${style.linksContainer}`}>
-                    <Link className={style.link} to="/">
-                        Home
-                    </Link>
-                    <Link className={style.link} to="/group-lessons">
-                        Group Lessons
-                    </Link>
-                    <Link className={style.link} to="/private-lessons">
-                        Private Lessons
-                    </Link>
-                    <Link className={style.link} to="/contact">
-                        Contact
-                    </Link>
-                </div>
-            ) : null}
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setDisplayNavbar(!displayNavbar);
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.upperRow}>
+        <Link className={`${style.logo}`} to="/">
+          Codeocity
+        </Link>
+        <div
+          className={`${style.burgerMenu} ${isOpen ? style.change : ""}`}
+          onClick={handleClick}
+        >
+          <div className={style.bar}></div>
+          <div className={style.bar}></div>
+          <div className={style.bar}></div>
         </div>
-    );
+      </div>
+      {width > 825 || displayNavbar ? (
+        <div className={`${style.linksContainer}`}>
+          <Link className={style.link} to="/">
+            Home
+          </Link>
+          <Link className={style.link} to="/group-lessons">
+            Group Lessons
+          </Link>
+          <Link className={style.link} to="/private-lessons">
+            Private Lessons
+          </Link>
+          <Link className={style.link} to="/contact">
+            Contact
+          </Link>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default Navbar;
