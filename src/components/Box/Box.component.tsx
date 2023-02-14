250bede5a46975708e765142392e3cdddf480c7c
Brian Mason <brian.kw.mason@gmail.com>
2023-02-14 14:19:29 -0500

    Minor changes

--- src/components/Box/Box.component.tsx
+++ src/components/Box/Box.component.tsx
@@ -5,32 +5,30 @@ import type { IconType } from "react-icons/lib/esm/iconBase";
 import styles from "./Box.module.css";
 
 type Props = {
-    Icon: IconType;
-    title: string;
-    description: string;
-    background: string;
-    className?: string;
+  Icon: IconType;
+  title: string;
+  description: string;
+  background: string;
 };
 
 const Box: React.FC<Props> = ({
-    Icon,
-    title,
-    description,
-    background,
-    className,
+  Icon,
+  title,
+  description,
+  background,
 }: Props) => {
-    return (
-        <div className={`${styles.box} ${className}`}>
-            <Icon
-                className={styles.icon}
-                size={"2.8rem"}
-                color="white"
-                style={{ backgroundColor: `#${background}` }}
-            />
-            <h2 className={styles.title}>{title}</h2>
-            <p className={styles.description}>{description}</p>
-        </div>
-    );
+  return (
+    <div className={`${styles.box}`}>
+      <Icon
+        className={styles.icon}
+        size={"2.8rem"}
+        color="white"
+        style={{ backgroundColor: `#${background}` }}
+      />
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.description}>{description}</p>
+    </div>
+  );
 };
 
 export default Box;
--- src/pages/Home/Why/Why.page.tsx
+++ src/pages/Home/Why/Why.page.tsx
@@ -5,36 +5,33 @@ import Box from "../../../components/Box/Box.component";
 import styles from "./Why.module.css";
 
 const Why = () => {
-    return (
-        <>
-            <h1 className={styles.title}>Why Learn to Code?</h1>
-            <div className={styles.boxes}>
-                <Box
-                    Icon={FaBrain}
-                    title="Thinking"
-                    description="Coding is a very logic based activity. By learning to code, you sharpen your analytical reasoning and logic skills."
-                    background="9747FF"
-                    className={styles.box}
-                />
+  return (
+    <>
+      <h1 className={styles.title}>Why Learn to Code?</h1>
+      <div className={styles.boxes}>
+        <Box
+          Icon={FaBrain}
+          title="Thinking"
+          description="Coding is a very logic based activity. By learning to code, you sharpen your analytical reasoning and logic skills."
+          background="9747FF"
+        />
 
-                <Box
-                    Icon={FaWrench}
-                    title="Problem Solving"
-                    description="Coding is a very logic based activity. By learning to code, you sharpen your analytical reasoning and logic skills."
-                    background="ABCD51"
-                    className={styles.box}
-                />
+        <Box
+          Icon={FaWrench}
+          title="Problem Solving"
+          description="Coding is a very logic based activity. By learning to code, you sharpen your analytical reasoning and logic skills."
+          background="ABCD51"
+        />
 
-                <Box
-                    Icon={FaRegSmileBeam}
-                    title="It's fun!"
-                    description="Coding is a very logic based activity. By learning to code, you sharpen your analytical reasoning and logic skills."
-                    background="FF5FA7"
-                    className={styles.box}
-                />
-            </div>
-        </>
-    );
+        <Box
+          Icon={FaRegSmileBeam}
+          title="It's fun!"
+          description="Coding is a very logic based activity. By learning to code, you sharpen your analytical reasoning and logic skills."
+          background="FF5FA7"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Why;
