894a9b44f984e835b16d1e065578eb1fa0dc4e7d
Yixuan Li <lxd520ANNA@gmail.com>
2023-02-19 12:21:10 -0500

    more box fixes + start group lessons, private lessons, and contact pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home/Home.page";
 import "./App.css";
+import GroupLessons from "./pages/GroupLessons/GroupLessons.page";
+import PrivateLessons from "./pages/PrivateLessons/PrivateLessons.page";
+import Contact from "./pages/Contact/Contact.page";
 
 function App() {
     return (
@@ -11,7 +14,15 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Layout />}>
                         <Route index element={<Home />} />
-                        {/* <Route path="" element={} /> */}
+                        <Route
+                            path="/group-lessons"
+                            element={<GroupLessons />}
+                        />
+                        <Route
+                            path="/private-lessons"
+                            element={<PrivateLessons />}
+                        />
+                        <Route path="/contact" element={<Contact />} />
                         {/* <Route path="*" element={} /> */}
                         {/* ^^ back up not found page */}
                     </Route>
---
+++ src/pages/Contact/Contact.page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+
+type Props = {};
+
+const Contact = (props: Props) => {
+    return <div>Contact</div>;
+};
+
+export default Contact;
