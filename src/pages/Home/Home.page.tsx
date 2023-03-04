042ead27208184e7eb6e8450dc61bbbd125141a1
Yixuan Li <lxd520ANNA@gmail.com>
2023-03-03 22:58:05 -0500

    small fixes

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -6,21 +6,20 @@ import { Banner } from "./Banner/Banner.page";
 import { UniversalPadding } from "../../components/UniversalPadding/UniversalPadding.component";
 import { Reviews } from "./Reviews/Reviews.page";
 import { Lessons } from "./Lessons/Lessons.page";
-import Why from "./Why/Why.page";
 
 const Home = () => {
-  return (
-    <div className={styles.homeContainer}>
-      <UniversalPadding>
-        <Landing kid={kid} />
-      </UniversalPadding>
-      <Banner />
-      <UniversalPadding>
-        <Reviews />
-        <Lessons />
-      </UniversalPadding>
-    </div>
-  );
+    return (
+        <div className={styles.homeContainer}>
+            <UniversalPadding>
+                <Landing kid={kid} />
+            </UniversalPadding>
+            <Banner />
+            <UniversalPadding>
+                <Reviews />
+                <Lessons />
+            </UniversalPadding>
+        </div>
+    );
 };
 
 export default Home;
