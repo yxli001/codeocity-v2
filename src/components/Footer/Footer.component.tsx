f0762fa9f5bd75f5bfbf1091b86d8ddd13106884
Yixuan Li <lxd520ANNA@gmail.com>
2023-03-04 11:04:45 -0500

    fix footer

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -1,81 +1,66 @@
 import React from "react";
+import courses from "../../data/courses";
 import DropdownLinks from "./DropdownLinks/DropdownLinks";
 
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  const navigationLinks = [
-    {
-      name: "Home",
-      to: "/",
-    },
-    {
-      name: "Group Lessons",
-      to: "/group-lessons",
-    },
-    {
-      name: "Private Lessons",
-      to: "/private-lessons",
-    },
-    {
-      name: "Jobs",
-      to: "/jobs",
-    },
-    {
-      name: "Contact",
-      to: "/contact",
-    },
-  ];
+    const navigationLinks = [
+        {
+            name: "Home",
+            to: "/",
+        },
+        {
+            name: "Group Lessons",
+            to: "/group-lessons",
+        },
+        {
+            name: "Private Lessons",
+            to: "/private-lessons",
+        },
+        {
+            name: "Jobs",
+            to: "/jobs",
+        },
+        {
+            name: "Contact",
+            to: "/contact",
+        },
+    ];
 
-  const coursesLinks = [
-    {
-      name: "Scratch",
-      to: "/courses/1",
-    },
-    {
-      name: "Web Dev 1",
-      to: "/courses/2",
-    },
-    {
-      name: "Advanced CSS",
-      to: "/courses/4",
-    },
-    {
-      name: "Intro to JavaScript",
-      to: "/courses/3",
-    },
-    {
-      name: "DOM Manipulation",
-      to: "/courses/5",
-    },
-  ];
+    const coursesLinks = courses.map((course) => {
+        return {
+            name: course.title,
+            to: `/courses/${course.id}`,
+        };
+    });
 
-  return (
-    <div className={styles.footer}>
-      <div className={styles.info}>
-        <div className={styles.title}>Codeocity</div>
-        <a
-          href="mailto:contact@codeocity.org"
-          target="_blank"
-          className={styles.email}
-          rel="noreferrer"
-        >
-          contact@codeocity.org
-        </a>
-      </div>
-      <DropdownLinks
-        dropdownTitle="Navigate"
-        links={navigationLinks}
-        className={styles.navigate}
-      />
-      <DropdownLinks
-        dropdownTitle="Courses"
-        links={coursesLinks}
-        className={styles.courses}
-      />
-      <div className={styles.copyright}>&copy; 2023 Codeocity</div>
-    </div>
-  );
+    return (
+        <div className={styles.footer}>
+            <div className={styles.info}>
+                <div className={styles.title}>Codeocity</div>
+                <a
+                    href="mailto:contact@codeocity.org"
+                    target="_blank"
+                    className={styles.email}
+                    rel="noreferrer"
+                >
+                    contact@codeocity.org
+                </a>
+            </div>
+            <DropdownLinks
+                dropdownTitle="Navigate"
+                links={navigationLinks}
+                className={styles.navigate}
+            />
+            <DropdownLinks
+                dropdownTitle="Courses"
+                links={coursesLinks}
+                className={styles.courses}
+            />
+            <div className={styles.copyright}>&copy; 2023 Codeocity</div>
+        </div>
+    );
 };
 
 export default Footer;
